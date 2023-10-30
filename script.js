f81362811cc1ead14c3bf104708fb57f3b4db448
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-10-30 16:15:41 -0500

    formatting

--- script.js
+++ script.js
@@ -4,17 +4,15 @@
 // let fungusHP = 100;
 
 function onReady() {
-    console.log("Ready to go!")
-    
-    // Make sure you check the index.html file! 
-    // There are lots of buttons and things ready for you to hook into here!
-    
-    
-    // 🧠 Remember
-    // - Handle events that ->
-    // - Updates state which is ->
-    // - Rendered to the DOM
-}
+  console.log("Ready to go!")
 
+  // Make sure you check the index.html file!
+  // There are lots of buttons and things ready for you to hook into here!
+
+  // 🧠 Remember
+  // - Handle events that ->
+  // - Updates state which is ->
+  // - Rendered to the DOM
+}
 
 onReady()
