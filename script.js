d28ae0224a5288faa61585ec6655396f5f464864
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-10-30 16:25:28 -0500

    Added basic attack handling

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // Feel free to make this to what you want!
 // Example:
 // let fungusHP = 100;
+let fungusHP = 100
+let playerAP = 100
 
 function onReady() {
   console.log("Ready to go!")
@@ -13,6 +15,34 @@ function onReady() {
   // - Handle events that ->
   // - Updates state which is ->
   // - Rendered to the DOM
+}
+
+function attack(apCost, damage) {
+  playerAP -= apCost
+  fungusHP -= damage
+  if (playerAP < 0) {
+    playerAP = 0
+  }
+  if (fungusHP < 0) {
+    fungusHP = 0
+  }
+  console.log(`Player AP: ${playerAP}, Fungus HP: ${fungusHP}`)
+}
+
+function arcaneScepter() {
+  attack(12, 14)
+}
+
+function entangle() {
+  attack(23, 9)
+}
+
+function dragonBlade() {
+  attack(38, 47)
+}
+
+function starFire() {
+  attack(33, 25)
 }
 
 onReady()
